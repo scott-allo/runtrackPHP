758e4e2208c036eb5ed35c1575c221490b401f4d
scott <scott.allo@laplateforme.io>
2025-01-13 14:25:36 +0100

    Fin du job 01, compréhension de str, val et vrai ou faux

---
+++ Jour01/job01/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$str = "LaPlateforme";
+
+
+$str2 = "Vive";
+
+
+$str3 = "!";
+echo $str2, " ", $str, " ", $str3;
+
+
+$val = 6;
+echo $val;
+$val2 =4;
+echo $val + $val2;
+
+
+$myBool = true;
+echo  $myBool ? 'true' : 'false' ;
+
+$myBool = false;
+echo $myBool ? 'true' : 'false' ;
+
+
