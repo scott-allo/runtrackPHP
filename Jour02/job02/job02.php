a21e2c099e22b2bc3a7ec56fac5e5b162a6c13aa
scott <scott.allo@laplateforme.io>
2025-01-14 10:39:17 +0100

    Job02 terminé, boucle while et utilisation des conditions

---
+++ Jour02/job02/job02.php
@@ -0,0 +1,26 @@
+<?php
+
+$nombre = 0;
+
+while($nombre <= 1337){
+
+    if($nombre == 26){
+        echo "<br>";}
+
+    elseif($nombre == 37){
+        echo "<br>";}
+
+    elseif($nombre == 88){
+            echo "<br>";}
+
+    elseif($nombre == 1111){
+                echo "<br>";}
+
+    else{echo $nombre , "<br>";}
+
+    $nombre ++ ;
+
+    }
+
+
+?>
