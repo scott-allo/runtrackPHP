6635b6028133c413e187fb1c63a2b6015d56d7b9
scott <scott.allo@laplateforme.io>
2025-01-14 10:24:37 +0100

    Début du jour02 et fin du job01 boucle while

---
+++ Jour02/job01.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre = 0;
+
+while($nombre <= 1337){
+    if($nombre == 42){
+        echo"<strong>$nombre</strong><br>";}
+    else{
+        echo $nombre,"<br>";
+    }
+    $nombre ++ ;
+
+}
+?>
