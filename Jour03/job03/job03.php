7f8fa8b234e6d258ae2afa5bb8bebeb3a161aa0a
scott <scott.allo@laplateforme.io>
2025-01-15 15:04:39 +0100

    Correction du job03  du jour03 sans fonction systeme,utilisation de while et de isset

--- Jour03/job03/job03.php
+++ Jour03/job03/job03.php
@@ -1,16 +1,20 @@
 <?php
 
-$str="I'm sorry Dave I'm afraid I can't do that.";
-$long=strlen($str);
+$str = "I'm sorry Dave I'm afraid I can't do that.";
+$i = 0;
 
-
-for($i = 0 ; $i<$long ; $i ++){
-    $voy= strtolower($str[$i]); 
-    if($voy== "a"||$voy=="e"||$voy=="i"||$voy=="o"||$voy=="u"||$voy=="y"){
-    echo $str[$i];
+while (isset($str[$i])) {
+    $voy = $str[$i]; 
+    
+  
+    if ($voy == "a" || $voy == "e" || $voy == "i" || $voy == "o" || $voy == "u" || $voy == "y" || 
+        $voy == "A" || $voy == "E" || $voy == "I" || $voy == "O" || $voy == "U" || $voy == "Y") {
+        echo $str[$i];  
     }
+    
+    $i++; 
 }
-
+?>
 
 
 
