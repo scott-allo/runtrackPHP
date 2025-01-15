c4875adf807e2e6ddb9f242de6bca729d91a7749
scott <scott.allo@laplateforme.io>
2025-01-15 11:02:22 +0100

    Début du jour03, Job01 terminé, Tableau et utilisation de foreach

---
+++ Jour03/job01/job01.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$tableau =[200,204,173,98,171,404,459];
+foreach ($tableau as $valeur) {
+    if ($valeur % 2 == 0) {
+        echo "$valeur est paire<br />";
+    } else {
+        echo "$valeur est impaire<br />";
+    }
+}
